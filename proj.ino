e49a92f2b5bd24d3d7cd5fe926e6e1f48d071aa0
Keshav Singh <keshavsingh200100@gmail.com>
2019-10-29 00:13:11 +0530

    update

---
+++ proj.ino
@@ -0,0 +1,49 @@
+
+
+#include <LiquidCrystal.h>
+int serial;
+char array1[]="     keshav      ";                //the string to print on the LCD
+char array2[]="  hello boss!                       ";                  //the string to print on the LCD
+char array3[]="   hi  bhai         ";                //the string to print on the LCD
+
+int tim = 250;  //the value of delay time
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
+
+void setup()
+{ Serial.begin(9600);
+  lcd.begin(16, 2);  // set up the LCD's number of columns and rows: 
+}
+
+void loop() 
+{   if(Serial.available()>0){
+      serial=Serial.read();
+      delay(1000);
+      Serial.println(serial);
+      if(serial=='1'){
+    lcd.clear();  //Clears the LCD screen and positions the cursor in the upper-left corner
+    lcd.setCursor(15,0);                   // set the cursor to column 15, line 1
+    for (int positionCounter2 = 0; positionCounter2 < 30; positionCounter2++)
+    {
+      lcd.scrollDisplayLeft();             //Scrolls the contents of the display one space to the left.
+      lcd.print(array2[positionCounter2]); // Print a message to the LCD.
+      delay(tim);                          //wait for 250 microseconds
+    }
+
+    lcd.clear();  //Clears the LCD screen and positions the cursor in the upper-left corner.   
+    
+    lcd.setCursor(0,0);                    // set the cursor to column 15, line 0
+    for (int positionCounter1 = 0; positionCounter1 < 16; positionCounter1++)
+    {
+      lcd.print(array1[positionCounter1]); // Print a message to the LCD.
+      delay(tim);                          //wait for 250 microseconds
+    }
+    
+    lcd.setCursor(0,1);                    // set the cursor to column 15, line 1
+    for (int positionCounter3 = 0; positionCounter3 < 16; positionCounter3++)
+    {
+      lcd.print(array3[positionCounter3]); // Print a message to the LCD.
+      delay(tim);                          //wait for 250 microseconds
+    }}}
+}
